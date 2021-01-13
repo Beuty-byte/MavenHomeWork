28a1fb1a9590de9e75c9bed894841c1fcd71fb2f
Andrei Birukou <medialabs91@gmail.com>
2021-01-13 22:16:29 +0300

    Logging commit

--- src/main/java/jsonParser/App.java
+++ src/main/java/jsonParser/App.java
@@ -1,23 +1,37 @@
 package jsonParser;
 
-
-
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 
 import java.io.*;
 import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 public class App {
-    public static void main(String[] args) throws IOException {
 
+    private static final Logger logger = Logger.getLogger(App.class.getName());
+
+    public static void main(String[] args){
+
+        logger.info("Start Application");
 
         String configJsonFile = args[0];
         String fileToChange = args[1];
 
         JsonParser parser = new JsonParser();
-        JsonObject root = parser.parse(new FileReader(configJsonFile)).getAsJsonObject();
+        JsonObject root = null;
+        try {
+            root = parser.parse(new FileReader(configJsonFile)).getAsJsonObject();
+            logger.info("Config file read " + root.toString());
+        } catch (FileNotFoundException e) {
+            logger.log(Level.SEVERE,"Exception file not found ",e);
+            e.printStackTrace();
+        }
+
+
         String suffix = root.get("suffix").toString();
         suffix = suffix.replaceAll("\"","");
         System.out.println("suffix = " + suffix);
@@ -39,9 +53,9 @@ public class App {
 
 
             filesList[i].renameTo(new File(fileToChange + buf));
-
+            logger.info("file rename " + filesList[i].getName());
         //mvn exec:java -Dexec.args="C:\Users\Wizzi\Desktop\forExample\Conf.json C:\Users\Wizzi\Desktop\filesForChenge\ "
         }
-
+        logger.info("Finish application");
     }
 }
